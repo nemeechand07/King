d6f809453e7486f008f11fc591b94e6a2961eea6
nemeechand07 <135504538+nemeechand07@users.noreply.github.com>
2023-06-05 07:29:03 +0530

    Update [...postpath].tsx

--- Pages/[...postpath].tsx
+++ Pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint =https://nemeechand.000webhostapp.com/graphql process.env.GRAPHQL_ENDPOINT as string;
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
